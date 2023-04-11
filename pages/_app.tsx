f2393629c8f17009646226cbb92044adb9943fe4
Muhammad zulqarnain <zulqarnainniazi95@gmail.com>
2023-04-11 02:57:00 -0700

    completed login and signup page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import "../styles/fonts.css";
+import type { AppProps } from "next/app";
+import Layout from "../layout/layout";
+import { AuthProvider } from "../hooks/useAuth";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <AuthProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </AuthProvider>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./layout/**/*.{js,ts,jsx,tsx}",
+    "./views/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        brand: {
+          main: "#016777",
+        },
+        black: {
+          main: "#000000",
+          abbey: "#494F55",
+        },
+        grey:{
+          main:"#C4C4C4"
+        }
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+      boxShadow: {
+        customShadow:
+          "rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px",
       },
     },
   },
   plugins: [],
-}
+};
