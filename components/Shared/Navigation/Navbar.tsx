4d394ee06f746618987a6339efbd32c0e5c68624
Muhammad zulqarnain <zulqarnainniazi95@gmail.com>
2023-04-11 03:58:54 -0700

    completed hero section

--- components/Shared/Navigation/Navbar.tsx
+++ components/Shared/Navigation/Navbar.tsx
@@ -4,6 +4,11 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import Button from '../Buttons/Button';
+import { RxHamburgerMenu } from 'react-icons/rx';
+import { TfiClose } from 'react-icons/tfi';
+import Drawer from 'react-modern-drawer'
+import 'react-modern-drawer/dist/index.css'
+
 
 const Menus = [
     {
@@ -11,7 +16,7 @@ const Menus = [
         Route: "/"
     },
     {
-        Name: "About US",
+        Name: "About Us",
         Route: ""
     },
     {
@@ -26,11 +31,15 @@ const Menus = [
 
 const Navbar = () => {
     const Router = useRouter();
+    const [isOpen, setIsOpen] = React.useState(false)
+    const toggleDrawer = () => {
+        setIsOpen((prevState) => !prevState)
+    }
     return (
-        <ComponentWrapper style='h-[80px] bg-brand-main' >
+        <ComponentWrapper style='md:h-[80px] h-[60px] bg-brand-main' >
             <div className='w-full h-full flex justify-between items-center' >
                 {/* Logo */}
-                <div className='w-[50px] h-[50px] relative ' >
+                <div className='md:w-[50px] w-[40px] md:h-[50px] h-[40px] relative ' >
                     <Image src={"/logo.svg"} alt='' fill className='object-contain' />
                 </div>
 
@@ -52,7 +61,40 @@ const Navbar = () => {
                         </Link>
                     </div>
                 </div>
+
+                {/* hamburger */}
+                <RxHamburgerMenu onClick={toggleDrawer} className='md:hidden block text-[30px] cursor-pointer text-white' />
             </div>
+            <Drawer
+                open={isOpen}
+                onClose={toggleDrawer}
+                direction='left'
+                style={{ width: "100%" }}
+            >
+                <div className='w-full h-full bg-brand-main' >
+                    <div className='h-[60px] w-full flex justify-between items-center px-4' >
+                        <Image src={"/logo.svg"} alt='' width={40} height={40} className='object-contain' />
+                        <TfiClose onClick={toggleDrawer} className='md:hidden block text-[24px] cursor-pointer text-white' />
+                    </div>
+                    <div className='w-full flex justify-center items-center flex-col gap-4 mt-8' >
+                        {
+                            Menus?.map((item: any, index: number) => {
+                                return <Link key={index} href={item.Route} className={`font-inter font-medium text-[14px] leading-[32px] tracking-[0.2rem] text-white relative after:absolute after:bottom-0 after:left-0 after:h-[2px] after:bg-white after:w-0 after:transition-all after:duration-300 hover:after:w-[100%] ${item.Route === Router.pathname && "after:w-[100%]"} `} >
+                                    {item.Name}
+                                </Link>
+                            })
+                        }
+                        <div className='flex justify-center items-center gap-3 mt-6' >
+                            <Link href={"/login"} >
+                                <Button Text='Login' variant='contained' style='w-[120px] h-[48px] text-black-abbey bg-white' />
+                            </Link>
+                            <Link href={"/signup"} >
+                                <Button Text='Sign Up' variant='outlined' style='w-[120px] h-[48px] text-white border-white' />
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </Drawer>
         </ComponentWrapper>
     )
 }
